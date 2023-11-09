d0dcfd704b6e18909df2599512d6a84d598dd057
Aditya <bhandariaditya2004@gmail.com>
2023-11-09 13:44:42 +0530

    Data Ingestion Completed

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("C:/Users/bhand/OneDrive/Desktop/Deep Learning/Chicken_disease/Chicken-Disease-Classification-Tutorial/config/config.yaml")
+PARAMS_FILE_PATH = Path("C:/Users/bhand/OneDrive/Desktop/Deep Learning/Chicken_disease/Chicken-Disease-Classification-Tutorial/params.yaml")
+
+# print(CONFIG_FILE_PATH)
